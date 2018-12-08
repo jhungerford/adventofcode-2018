7670bf14bf2bc30b4b1747d0916cab824017d9cf
John Hungerford <jhungerford@gmail.com>
2018-12-08 08:27:13 -0700

    Day 7 part 2.

--- src/main/java/dev/jh/adventofcode/Day7.java
+++ src/main/java/dev/jh/adventofcode/Day7.java
@@ -1,9 +1,8 @@
 package dev.jh.adventofcode;
 
 import com.google.common.base.Charsets;
-import com.google.common.base.MoreObjects;
-import com.google.common.base.Objects;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import com.google.common.io.Files;
@@ -15,48 +14,51 @@ import java.util.regex.Pattern;
 
 public class Day7 {
 
-  public static class Step {
-    public final char name;
-    public final ImmutableSet<Character> prerequisites;
-    public final ImmutableSet<Step> nextSteps;
+  /**
+   * Maps containing restrictions on which steps must run before and after other steps.
+   */
+  private static class Steps {
+    public final ImmutableMap<Character, Set<Character>> stepToNextMap;
+    public final ImmutableMap<Character, Set<Character>> stepToPreviousMap;
 
-    public Step(char name, ImmutableSet<Character> prerequisites, ImmutableSet<Step> nextSteps) {
-      this.name = name;
-      this.prerequisites = prerequisites;
-      this.nextSteps = nextSteps;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-      Step step = (Step) o;
-      return name == step.name &&
-          Objects.equal(nextSteps, step.nextSteps);
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hashCode(name, nextSteps);
-    }
-
-    @Override
-    public String toString() {
-      return MoreObjects.toStringHelper(this)
-          .add("name", name)
-          .add("nextSteps", nextSteps)
-          .toString();
+    public Steps(
+        ImmutableMap<Character, Set<Character>> stepToNextMap,
+        ImmutableMap<Character, Set<Character>> stepToPreviousMap
+    ) {
+      this.stepToNextMap = stepToNextMap;
+      this.stepToPreviousMap = stepToPreviousMap;
     }
   }
 
   /**
-   * Parses the given list of step restrictions, returning the order that teh steps should be run in. If the
-   * prerequisites for more than one step have been met, the step with the lowest alphabetical order goes first.
-   *
-   * @param lines Lines to parse - one restriction per line
-   * @return Order steps should be performed in.
+   * Step and the number of seconds it takes to run that step.
    */
-  public static String order(ImmutableList<String> lines) {
+  private static class StepSeconds {
+    private final char step;
+    private final int seconds;
+
+    public StepSeconds(char step) {
+      this.step = step;
+      this.seconds = 60 + (step - 'A' + 1);
+    }
+
+    public StepSeconds(char step, int seconds) {
+      this.step = step;
+      this.seconds = seconds;
+    }
+
+    public StepSeconds subtract(int seconds) {
+      return new StepSeconds(step, this.seconds - seconds);
+    }
+  }
+
+  /**
+   * Parses the given list of step restrictions.
+   *
+   * @param lines Lines to parse - one restriction per line.
+   * @return Steps to complete the work described in the lines.
+   */
+  public static Steps parseLines(ImmutableList<String> lines) {
     Pattern pattern = Pattern.compile("Step ([A-Z]) must be finished before step ([A-Z]) can begin.");
 
     // Rip through the steps, building maps of step names -> next step names and step names -> previous step names
@@ -74,9 +76,23 @@ public class Day7 {
       }
     }
 
+    return new Steps(
+        ImmutableMap.copyOf(stepToNextMap),
+        ImmutableMap.copyOf(stepToPreviousMap)
+    );
+  }
+
+  /**
+   * Returns the order that the steps should be run in. If the prerequisites for more than one step have been met,
+   * the step with the lowest alphabetical order goes first.
+   *
+   * @param steps Step restrictions.
+   * @return Order steps should be performed in.
+   */
+  public static String order(Steps steps) {
     // Looking at which elements are in one map and not the other, stepToNextMap's uniques are the first steps
     PriorityQueue<Character> nextSteps = new PriorityQueue<>(Comparator.naturalOrder());
-    nextSteps.addAll(Sets.difference(stepToNextMap.keySet(), stepToPreviousMap.keySet()));
+    nextSteps.addAll(Sets.difference(steps.stepToNextMap.keySet(), steps.stepToPreviousMap.keySet()));
 
     Set<Character> done = new HashSet<>();
     StringBuilder order = new StringBuilder();
@@ -85,19 +101,77 @@ public class Day7 {
       done.add(stepName);
       order.append(stepName);
 
-      stepToNextMap.getOrDefault(stepName, ImmutableSet.of()).stream()
-          .filter(nextStepName -> done.containsAll(stepToPreviousMap.get(nextStepName)))
+      steps.stepToNextMap.getOrDefault(stepName, ImmutableSet.of()).stream()
+          .filter(nextStepName -> done.containsAll(steps.stepToPreviousMap.get(nextStepName)))
           .forEach(nextSteps::add);
     }
 
     return order.toString();
   }
 
+  /**
+   * Returns the amount of time that it takes to complete all of the steps with the given number of workers.
+   * Each step takes 60 seconds plus an amount corresponding to the letter - A=1, B=2, etc.  If the prerequisites
+   * for more than one step have been completed, workers grab steps in alphabetical order.
+   *
+   * @param steps Step restrictions
+   * @param numWorkers Number of workers that can perform steps concurrently
+   * @return Number of seconds it takes the workers to complete all of the steps
+   */
+  public static int time(Steps steps, int numWorkers) {
+    PriorityQueue<Character> nextSteps = new PriorityQueue<>(Comparator.naturalOrder());
+    nextSteps.addAll(Sets.difference(steps.stepToNextMap.keySet(), steps.stepToPreviousMap.keySet()));
+
+    int totalSeconds = 0;
+
+    StepSeconds[] workers = new StepSeconds[numWorkers];
+    Set<Character> done = new HashSet<>();
+
+    while (! nextSteps.isEmpty() || Arrays.stream(workers).anyMatch(Objects::nonNull)) {
+      // Idle workers pick up work
+      for (int worker = 0; worker < numWorkers && !nextSteps.isEmpty(); worker++) {
+        if (workers[worker] == null) {
+          workers[worker] = new StepSeconds(nextSteps.remove());
+        }
+      }
+
+      // Determine which step will be completed next.
+      StepSeconds completed = Arrays.stream(workers)
+          .filter(Objects::nonNull)
+          .min(Comparator.comparing(stepSeconds -> stepSeconds.seconds))
+          .orElseThrow(() -> new IllegalStateException("No active workers with work remaining."));
+
+      totalSeconds += completed.seconds;
+
+      // Tick down all of the ongoing work
+      for (int worker = 0; worker < numWorkers; worker ++) {
+        StepSeconds work = workers[worker];
+        if (work == null) {
+          // Nothing to do - idle worker.
+
+        } else if (work.seconds == completed.seconds) {
+          workers[worker] = null;
+          done.add(work.step);
+          steps.stepToNextMap.getOrDefault(work.step, ImmutableSet.of()).stream()
+              .filter(nextStepName -> done.containsAll(steps.stepToPreviousMap.get(nextStepName)))
+              .forEach(nextSteps::add);
+
+        } else {
+          workers[worker] = work.subtract(completed.seconds);
+        }
+      }
+    }
+
+    return totalSeconds;
+  }
+
   public static void main(String[] args) throws Exception {
     File file = new File(Day7.class.getResource("/day7.txt").getFile());
     ImmutableList<String> lines = ImmutableList.copyOf(Files.readLines(file, Charsets.UTF_8));
+    Steps steps = parseLines(lines);
 
     // Part 1: what order should the steps be completed in?
-    System.out.println("Part 1: " + order(lines));
+    System.out.println("Part 1: " + order(steps));
+    System.out.println("Part 2: " + time(steps, 5));
   }
 }
--- src/test/java/dev/jh/adventofcode/Day7Test.java
+++ src/test/java/dev/jh/adventofcode/Day7Test.java
@@ -7,19 +7,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class Day7Test {
 
+  private static final ImmutableList<String> LINES = ImmutableList.of(
+      "Step C must be finished before step A can begin.",
+      "Step C must be finished before step F can begin.",
+      "Step A must be finished before step B can begin.",
+      "Step A must be finished before step D can begin.",
+      "Step B must be finished before step E can begin.",
+      "Step D must be finished before step E can begin.",
+      "Step F must be finished before step E can begin.",
+      "Step A must be finished before step E can begin."
+  );
+
   @Test
   public void order() {
-    ImmutableList<String> lines = ImmutableList.of(
-        "Step C must be finished before step A can begin.",
-        "Step C must be finished before step F can begin.",
-        "Step A must be finished before step B can begin.",
-        "Step A must be finished before step D can begin.",
-        "Step B must be finished before step E can begin.",
-        "Step D must be finished before step E can begin.",
-        "Step F must be finished before step E can begin.",
-        "Step A must be finished before step E can begin."
-    );
+    assertThat(Day7.order(Day7.parseLines(LINES))).isEqualTo("CABDFE");
+  }
 
-    assertThat(Day7.order(lines)).isEqualTo("CABDFE");
+  @Test
+  public void time() {
+    assertThat(Day7.time(Day7.parseLines(LINES), 2)).isEqualTo(258);
   }
 }
