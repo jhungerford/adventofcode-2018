467033baa749e6a337f450324279eb9a37e00140
John Hungerford <jhungerford@gmail.com>
2019-01-06 08:16:18 -0700

    Day 23 part 2.

--- src/main/java/dev/jh/adventofcode/Day23.java
+++ src/main/java/dev/jh/adventofcode/Day23.java
@@ -4,17 +4,21 @@ import com.google.common.base.Charsets;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Objects;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.io.Files;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Comparator;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Day23 {
 
   public static class Position {
+    public static final Position ORIGIN = new Position(0, 0, 0);
+
     public final long x;
     public final long y;
     public final long z;
@@ -117,6 +121,49 @@ public class Day23 {
         .count();
   }
 
+
+
+  private static ImmutableMap<Nanobot, ImmutableSet<Nanobot>> buildVisibilityMap(ImmutableList<Nanobot> nanobots) {
+    Map<Nanobot, Set<Nanobot>> map = new HashMap<>();
+
+    for (int a = 0; a < nanobots.size(); a ++) {
+      for (int b = a + 1; b < nanobots.size(); b ++) {
+        Nanobot nanobotA = nanobots.get(a);
+        Nanobot nanobotB = nanobots.get(b);
+
+        if (nanobotA.inRange(nanobotB)) {
+          map.computeIfAbsent(nanobotA, (key) -> new HashSet<>()).add(nanobotB);
+        }
+
+        if (nanobotB.inRange(nanobotA)) {
+          map.computeIfAbsent(nanobotB, (key) -> new HashSet<>()).add(nanobotA);
+        }
+      }
+    }
+
+    return map.entrySet().stream().collect(ImmutableMap.toImmutableMap(
+        Map.Entry::getKey,
+        entry -> ImmutableSet.copyOf(entry.getValue())
+    ));
+  }
+
+  public static long mostNanobotsDistance(ImmutableList<Nanobot> nanobots) {
+    // Build up a graph of nanobots that can see each other
+    ImmutableMap<Nanobot, ImmutableSet<Nanobot>> visibilityMap = buildVisibilityMap(nanobots);
+
+    // Bron-Kerbosch to identify the maximal clique?
+    // Graph is directed, so doesn't apply - pick the bot that can see the most nodes
+    ImmutableSet<Nanobot> mostVisible = visibilityMap.values().stream()
+        .max(Comparator.comparing(bots -> bots.size()))
+        .orElseThrow(() -> new IllegalStateException("No nanobots in the visibility map."));
+
+    // Furthest nanobot sphere - closest point to the origin on it's sphere is the answer
+    return mostVisible.stream()
+        .mapToLong(nanobot -> nanobot.position.distance(Position.ORIGIN) - nanobot.range)
+        .max()
+        .orElseThrow(() -> new IllegalStateException("No nanobots in the maximal clique."));
+  }
+
   public static void main(String[] args) throws IOException {
     File file = new File(Day23.class.getResource("/day23.txt").getFile());
     ImmutableList<Nanobot> nanobots = Files.readLines(file, Charsets.UTF_8).stream()
@@ -125,5 +172,9 @@ public class Day23 {
 
     // Part 1: how many nanobots are in range of the nanobot with the largest signal radius?
     System.out.println("Part 1: " + mostBotsInRange(nanobots));
+
+    // Part 2: what is the shortest manhattan distance from the origin to the position
+    // in the radius of the largest number of nanobots?
+    System.out.println("Part 2: " + mostNanobotsDistance(nanobots));
   }
 }
--- src/test/java/dev/jh/adventofcode/Day23Test.java
+++ src/test/java/dev/jh/adventofcode/Day23Test.java
@@ -6,20 +6,31 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Day23Test {
-  private static final ImmutableList<Day23.Nanobot> EXAMPLE = ImmutableList.of(
-      Day23.Nanobot.parse("pos=<0,0,0>, r=4"),
-      Day23.Nanobot.parse("pos=<1,0,0>, r=1"),
-      Day23.Nanobot.parse("pos=<4,0,0>, r=3"),
-      Day23.Nanobot.parse("pos=<0,2,0>, r=1"),
-      Day23.Nanobot.parse("pos=<0,5,0>, r=3"),
-      Day23.Nanobot.parse("pos=<0,0,3>, r=1"),
-      Day23.Nanobot.parse("pos=<1,1,1>, r=1"),
-      Day23.Nanobot.parse("pos=<1,1,2>, r=1"),
-      Day23.Nanobot.parse("pos=<1,3,1>, r=1")
-  );
 
   @Test
   public void exampleInRange() {
-    assertThat(Day23.mostBotsInRange(EXAMPLE)).isEqualTo(7);
+    assertThat(Day23.mostBotsInRange(ImmutableList.of(
+        Day23.Nanobot.parse("pos=<0,0,0>, r=4"),
+        Day23.Nanobot.parse("pos=<1,0,0>, r=1"),
+        Day23.Nanobot.parse("pos=<4,0,0>, r=3"),
+        Day23.Nanobot.parse("pos=<0,2,0>, r=1"),
+        Day23.Nanobot.parse("pos=<0,5,0>, r=3"),
+        Day23.Nanobot.parse("pos=<0,0,3>, r=1"),
+        Day23.Nanobot.parse("pos=<1,1,1>, r=1"),
+        Day23.Nanobot.parse("pos=<1,1,2>, r=1"),
+        Day23.Nanobot.parse("pos=<1,3,1>, r=1")
+    ))).isEqualTo(7);
+  }
+
+  @Test
+  public void exampleMostDistance() {
+    assertThat(Day23.mostNanobotsDistance(ImmutableList.of(
+        Day23.Nanobot.parse("pos=<10,12,12>, r=2"),
+        Day23.Nanobot.parse("pos=<12,14,12>, r=2"),
+        Day23.Nanobot.parse("pos=<16,12,12>, r=4"),
+        Day23.Nanobot.parse("pos=<14,14,14>, r=6"),
+        Day23.Nanobot.parse("pos=<50,50,50>, r=200"),
+        Day23.Nanobot.parse("pos=<10,10,10>, r=5")
+    ))).isEqualTo(36);
   }
 }
